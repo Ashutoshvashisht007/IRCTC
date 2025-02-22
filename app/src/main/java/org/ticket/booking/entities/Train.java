36c595dd865c1610e76d7ebb76dd0e033df5722a
Ashutosh <ashuhina.123@gmail.com>
2025-02-22 12:06:45 +0530

    Signup and login done

--- app/src/main/java/org/ticket/booking/entities/Train.java
+++ app/src/main/java/org/ticket/booking/entities/Train.java
@@ -8,6 +8,59 @@ public class Train {
     private String trainId;
     private String trainNum;
     private List<List<Integer>> seats;
-    private Map<String, Time> stationTimes;
+    private Map<String, String> stationTimes;
     private List<String> stations;
+
+    public Train(String trainId,String trainNum,List<List<Integer>> seats,Map<String,String> stationTimes,List<String> stations){
+        this.trainId = trainId;
+        this.trainNum = trainNum;
+        this.seats = seats;
+        this.stationTimes = stationTimes;
+        this.stations = stations;
+    }
+
+    public String getTrainId(){
+        return  trainId;
+    }
+
+    public String getTrainNum(){
+        return trainNum;
+    }
+
+    public List<List<Integer>> getSeats(){
+        return seats;
+    }
+
+    public Map<String,String> getStationTimes(){
+        return stationTimes;
+    }
+
+    public List<String> getStations(){
+        return stations;
+    }
+
+    public void setTrainId(String trainId){
+        this.trainId = trainId;
+    }
+
+    public void setTrainNum(String trainNum){
+        this.trainNum = trainNum;
+    }
+
+    public void setSeats(List<List<Integer>> seats){
+        this.seats = seats;
+    }
+
+    public void setStationTimes(Map<String,String> stationTimes){
+        this.stationTimes = stationTimes;
+    }
+
+    public void setStations(List<String> stations){
+        this.stations = stations;
+    }
+
+    public String getTrainInfo(){
+        return String.format("Train ID: %s Train No: %s", trainId, trainNum);
+    }
+
 }
--- app/src/main/java/org/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/org/ticket/booking/services/UserBookingService.java
@@ -1,7 +1,9 @@
 package org.ticket.booking.services;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.ticket.booking.entities.Ticket;
 import org.ticket.booking.entities.User;
+import org.ticket.booking.utils.UserServiceUtil;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,7 +16,7 @@ public class UserBookingService {
     private List<User> userList;
 
     private ObjectMapper objectMapper = new ObjectMapper(); // to serialize or deserialize
-    private static final String USER_PATH = "../localDB/users.json";
+    private static final String USER_PATH = "app/src/main/java/org/ticket/booking/localDB/users.json";
 
     public UserBookingService(User user) throws IOException {
         this.user = user;
@@ -24,7 +26,7 @@ public class UserBookingService {
 
     public Boolean loginUser(){
         Optional<User> foundUser = userList.stream().filter(user1 -> {
-            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashPassword());
         }).findFirst();
 
         return foundUser.isPresent();
@@ -38,5 +40,37 @@ public class UserBookingService {
         }catch (IOException e){
             return Boolean.FALSE;
         }
+    }
+
+    private void saveUserListToFile() throws  IOException{
+        File usersFile = new File(USER_PATH);
+        objectMapper.writeValue(usersFile,userList);
+    }
+
+    public void fetchBooking(){
+        Optional<User> userFetched = userList.stream().filter(user1 -> {
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashPassword());
+        }).findFirst();
+        userFetched.ifPresent(User::printTickets);
+    }
+
+    public boolean cancelBooking(String ticketId){
+
+        if(ticketId == null || ticketId.isEmpty()){
+            System.out.println("TicketId cannot be null or empty");
+            return Boolean.FALSE;
+        }
+
+        String finalTicketId = ticketId;
+
+        boolean removed = user.getTicketBooked().removeIf(ticket -> ticket.getTicketId().equals(finalTicketId));
+
+        if(removed){
+            System.out.println("Ticket with id" + ticketId + " has been canceled successfully");
+            return Boolean.TRUE;
+        }
+
+        System.out.println("No ticket found with id " + ticketId);
+        return Boolean.FALSE;
     }
 }
